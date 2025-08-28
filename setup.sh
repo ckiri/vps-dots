c0239e3472560878b6b83bd5223099b1c02e515f
Chris Kiriakou <chris-kiriakou@web.de>
2025-08-28 12:34:59 +0200

    Add stow to packages to make stow command work.

--- setup.sh
+++ setup.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env sh
+#
+# Setup everything necessary for usage with a VPS 
 
 apt update -y && apt upgrade -y
-apt install tmux vim zsh ufw -y
+apt install foot-terminfo stow tmux vim zsh ufw -y
 
 chsh -s $(which zsh)
 
@@ -11,7 +13,7 @@ test ! -f ${HOME}/.cache/zsh/history && touch ${HOME}/.cache/zsh/history
 
 stow --dotfiles tmux vim zsh
 
-# Deny all everything but ssh
+# Deny everything but ssh -- don't f this up or your VPS is gone!
 ufw allow ssh
 ufw default deny incoming
 ufw default allow outgoing
